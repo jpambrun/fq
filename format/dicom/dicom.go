87810f8940948586e5fd83caf2a67c505128e2f7
JF Pambrun <jf.pambrun@gmail.com>
2022-01-09 14:44:28 -0500

    WIP: up to seq

--- format/dicom/dicom.go
+++ format/dicom/dicom.go
@@ -28,14 +28,23 @@ func decodeDicom(d *decode.D, in interface{}) interface{} {
 	d.FieldRawLen("File Preamble", 128*8)
 	d.FieldUTF8("DICOM Prefix", 4, d.AssertStr("DICM"))
 
-	for i := 0; i < 10; i++{
+	for i := 0; i < 100; i++{
 		tagBits := d.PeekBits(32)
-		tagString := fmt.Sprintf("x%X", tagBits)
+		tagString := fmt.Sprintf("x%08X", (tagBits >> 8 & 0x00FF00FF) | (tagBits << 8 & 0xFF00FF00))
 		d.FieldStruct(tagString, func(d *decode.D) {
-			d.FieldU32LE("tag1", scalar.Hex)
-			d.FieldUTF8("VR", 2)
-			d.FieldU16LE("length")
-			d.FieldU32LE("value")
+			d.FieldU16LE("group", scalar.Hex)
+			d.FieldU16LE("element", scalar.Hex)
+			vr := d.FieldUTF8("VR", 2)
+			// d.FieldU16LE("length")
+			// d.FieldU32LE("value")
+			var l uint64 = 0;
+			if(vr == "OB" || vr =="OW" || vr =="OF" || vr =="SQ" || vr =="UT" || vr =="UN"){
+				d.FieldRawLen("reserved", 16)
+				l = d.FieldU32LE("length")
+			} else {
+				l = d.FieldU16LE("length")
+			}
+			d.FieldRawLen("value", int64(l*8))
 		})
 	}
 	// d.FieldU32LE("tag2", scalar.Hex)
