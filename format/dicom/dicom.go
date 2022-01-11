6887f4519c836277d81b8d5c0f64c87f96e34dd3
JF Pambrun <jf.pambrun@gmail.com>
2022-01-10 21:46:40 -0500

    wip

--- format/dicom/dicom.go
+++ format/dicom/dicom.go
@@ -24,36 +24,40 @@ func init() {
 	})
 }
 
-func visit(d *decode.D){
+func visit(d *decode.D, parentItemCount int){
 	var itemCount = 0
 	for !d.End() {
 		tagBits := d.PeekBits(32)
+		tagString := fmt.Sprintf("x%08X", (tagBits >> 8 & 0x00FF00FF) | (tagBits << 8 & 0xFF00FF00))
 
-		if(tagBits == 0xfeff0de0){
-			d.FieldRawLen(fmt.Sprintf("item end%d", itemCount), 32)
-			d.FieldU32LE(fmt.Sprintf("item end (always 0)%d", itemCount))
+		if(tagBits == 0xfeff00e0){
+			//Item Tag
+			d.FieldRawLen("Item Tag", 32)
+			d.FieldU32LE("Item Len")
 			itemCount++
 			continue
 		}
-		if(tagBits == 0xfeffdde0){
-			d.FieldRawLen("seq end", 32)
-			d.FieldU32LE("seq end (always 0)")
+		if(tagBits == 0xfeff0de0){
+			//Item Delim.
+			d.FieldRawLen("Item Delim Tag", 32)
+			d.FieldU32LE("Item Delim Len")
 			return
 		}
-		if(tagBits == 0xfeff00e0){
-			d.FieldRawLen(fmt.Sprintf("item start%d", itemCount), 32)
-			d.FieldU32LE(fmt.Sprintf("item length%d", itemCount))
-			continue
+		if(tagBits == 0xfeffdde0){
+			//Seq. Delim.
+			d.FieldRawLen("Seq Delim Tag", 32)
+			d.FieldU32LE("Seq Delim Len")
+			return
 		}
 
-		tagString := fmt.Sprintf("x%08X", (tagBits >> 8 & 0x00FF00FF) | (tagBits << 8 & 0xFF00FF00))
+
 		d.FieldStruct(tagString, func(d *decode.D) {
 			d.FieldU16LE("group", scalar.Hex)
 			d.FieldU16LE("element", scalar.Hex)
-			vr := d.FieldUTF8("VR", 2)
+			vr := d.FieldUTF8("vr", 2)
 			var l uint64 = 0;
 			if(vr == "OB" || vr =="OW" || vr =="OF" || vr =="SQ" || vr =="UT" || vr =="UN"){
-				d.FieldRawLen("reserved", 16)
+				d.RawLen( 16)
 				l = d.FieldU32LE("length")
 			} else {
 				l = d.FieldU16LE("length")
@@ -62,7 +66,13 @@ func visit(d *decode.D){
 			if(vr == "SQ"){
 				// d.FieldRawLen("item start", 32)
 				// d.FieldU32LE("item length")
-				visit(d)
+				// visit(d, 0)
+				d.FieldArray("items", func(d *decode.D) { 
+					// for each item
+					d.FieldStruct("", func(d *decode.D) {
+						visit(d, itemCount) 
+					})
+				})
 			}
 			d.FieldRawLen("value", int64(l*8))
 		})
@@ -72,7 +82,7 @@ func visit(d *decode.D){
 func decodeDicom(d *decode.D, in interface{}) interface{} {
 	d.FieldRawLen("File Preamble", 128*8)
 	d.FieldUTF8("DICOM Prefix", 4, d.AssertStr("DICM"))
-	visit(d)
+	visit(d,0)
 	return nil
 }
 
