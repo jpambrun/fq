a0de823e0e38530b92af777bfc5326f2a372be42
JF Pambrun <jf.pambrun@gmail.com>
2022-01-10 20:19:44 -0500

    wip

--- format/dicom/dicom.go
+++ format/dicom/dicom.go
@@ -24,19 +24,23 @@ func init() {
 	})
 }
 
-func decodeDicom(d *decode.D, in interface{}) interface{} {
-	d.FieldRawLen("File Preamble", 128*8)
-	d.FieldUTF8("DICOM Prefix", 4, d.AssertStr("DICM"))
-
-	for i := 0; i < 100; i++{
+func visit(d *decode.D){
+	for !d.End() {
 		tagBits := d.PeekBits(32)
+
+		if(tagBits == 0xfeff0de0){
+			d.FieldRawLen("item end", 32)
+			continue
+		}
+		if(tagBits == 0xfeffdde0){
+			d.FieldRawLen("seq end", 32)
+			return
+		}
 		tagString := fmt.Sprintf("x%08X", (tagBits >> 8 & 0x00FF00FF) | (tagBits << 8 & 0xFF00FF00))
 		d.FieldStruct(tagString, func(d *decode.D) {
 			d.FieldU16LE("group", scalar.Hex)
 			d.FieldU16LE("element", scalar.Hex)
 			vr := d.FieldUTF8("VR", 2)
-			// d.FieldU16LE("length")
-			// d.FieldU32LE("value")
 			var l uint64 = 0;
 			if(vr == "OB" || vr =="OW" || vr =="OF" || vr =="SQ" || vr =="UT" || vr =="UN"){
 				d.FieldRawLen("reserved", 16)
@@ -44,37 +48,21 @@ func decodeDicom(d *decode.D, in interface{}) interface{} {
 			} else {
 				l = d.FieldU16LE("length")
 			}
+
+			if(vr == "SQ"){
+				d.FieldRawLen("item start", 32)
+				d.FieldU32LE("item length")
+				visit(d)
+			}
 			d.FieldRawLen("value", int64(l*8))
 		})
 	}
-	// d.FieldU32LE("tag2", scalar.Hex)
-	// d.FieldUTF8("VR2", 2)
-	// d.FieldU16LE("length2")
+}
 
-	// d.FieldU32LE("tag1", scalar.Hex)
-	// d.FieldUTF8("VR", 2)
-	// d.FieldU16LE("length")
-	// d.FieldU32LE("value")
-	// d.FieldU32LE("tag2", scalar.Hex)
-	// d.FieldUTF8("VR2", 2)
-	// d.FieldU16LE("length2")
-
-
-	
-	// d.FieldArray("tags", func(d *decode.D) {
-	// 	// for !d.End() {
-	// 	for i := 0; i < 10; i++{
-	// 		d.FieldStruct("tag", func(d *decode.D) {
-
-
-	// 		d.FieldU16LE("tagg", scalar.Hex)
-	// 		d.FieldU16LE("tag2", scalar.Hex)
-	// 		d.FieldUTF8("VR", 2)
-	// 		l := d.FieldU16LE("length")
-	// 		d.FieldRawLen("value", int64(l*8))
-	// 		})
-	// }
-// })
+func decodeDicom(d *decode.D, in interface{}) interface{} {
+	d.FieldRawLen("File Preamble", 128*8)
+	d.FieldUTF8("DICOM Prefix", 4, d.AssertStr("DICM"))
+	visit(d)
 	return nil
 }
 
